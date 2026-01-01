a406dd8fa16f57f256343c53eee2cdf3c211b598
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-01-01 21:30:49 +0530

    [Abhay] Add. Oops Level 01 code added

---
+++ oops-csharp-practice/gcr-codebase/level01/EmployeeDetails/MainProgram.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabz.oops_csharp_practice.level01.EmployeeDetails
+
+//{
+//    internal class MainProgram
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee employee = new Employee();
+//            employee.Input();
+//            employee.Display();
+//        }
+//    }
+//}
