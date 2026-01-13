0f1ab07c1e688abe82b4b7cc86a3d624b8e10090
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-01-13 14:28:14 +0530

    [Abhay] Add. Movie Schedule Manager Code Added

---
+++ dsa-csharp-practice/scenario-based/MovieScheduleManager/IMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.gcr_codebase.DSA.scenario_based.MovieScheduleManager
+{
+    internal interface IMovie
+    {
+        public void AddMovie(string title ,  string time);
+        public void SearchMovie(string keyWord);
+
+        public void Display();
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/MovieScheduleManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.gcr_codebase.DSA.scenario_based.MovieScheduleManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu m = new Menu();
+            m.Start();
+        }
+    }
+}
