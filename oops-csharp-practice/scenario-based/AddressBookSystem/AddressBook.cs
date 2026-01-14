6197fd401bdc10d427f0d241469f3a6de663783a
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-01-14 12:19:59 +0530

    [Abhay] Add. UC1 Completed

---
+++ oops-csharp-practice/scenario-based/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BridgeLabz.gcr_codebase.oops_csharp_practice.scenario_based.AddressBookSystem
+    {
+        internal class AddressBook
+        {
+            private Contact[] contacts;
+            private int Count;
+
+            public Contact[] GetContacts()
+            {
+                return contacts;
+            }
+
+            public void SetContacts(Contact[] contacts)
+            {
+            this.contacts = contacts;
+            }
+
+            public int GetCount()
+            {
+            return Count; 
+            }
+
+            public void SetCount()
+            {
+            this.Count = 0; 
+            }
+
+        public override string ToString()
+        {
+            return $"Contact : {contacts}";
+        }
+        }
+    }
+
+
