5d12a68c35fc78d61c0b041faa6072324f69e884
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-01-16 16:39:29 +0530

    [Abhay] Add. BookShelf

---
+++ dsa-csharp-practice/scenario-based/BookShelf/IBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.gcr_codebase.DSA.scenario_based.BookShelf
+{
+    internal interface IBook
+    {
+        void AddBook();
+        void DeleteBook();
+        void DisplayByGenre();
+        void UpdateStatus();
+
+    }
+}
