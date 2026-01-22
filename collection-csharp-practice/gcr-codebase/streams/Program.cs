9ba3ccd21798a2f144d120be5a57cab65a6330fa
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-01-22 20:54:01 +0530

    [Abhay] Add. Streams Assignment code Added

---
+++ collection-csharp-practice/gcr-codebase/streams/Program.cs
@@ -0,0 +1,6 @@
+namespace BridgeLabz.collection_csharp_practice.gcr_codebase.streams
+{
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+}
