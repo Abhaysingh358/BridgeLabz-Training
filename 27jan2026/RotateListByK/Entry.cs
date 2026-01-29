e8c8d3622d65fc6be777916517c9896d51510d7f
Abhaysingh358 <itsabhaypratapsingh03@gmail.com>
2026-01-29 11:43:07 +0530

    [Abhay] Add. Rotate linkedlist k times code added

---
+++ 27jan2026/RotateListByK/Entry.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace BridgeLabz.review.RotateListByK
+{
+    internal class Entry
+    {
+        LinkedList list = new LinkedList();
+
+        static void Main(string[] args)
+        {
+            Entry e = new Entry();
+            e.Menu();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine(
+                "1. Add First\n" +
+                "2. Display Before Rotation\n" +
+                "3. Rotate By K and Display\n" +
+                "0. Exit");
+
+            while (true)
+            {
+                Console.Write("\nEnter choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter element: ");
+                        int data = int.Parse(Console.ReadLine());
+                        list.AddFirst(data);
+                        break;
+
+                    case 2:
+                        list.DisplayBeforRotated();
+                        break;
+
+                    case 3:
+                        Console.Write("Enter K: ");
+                        int k = int.Parse(Console.ReadLine());
+                        list.RotateByK(k);
+                        Console.WriteLine("After Rotation:");
+                        list.DisplayBeforRotated();
+                        break;
+
+                    case 0:
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 27jan2026/RotateListByK/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BridgeLabz.review.RotateListByK
+{
+    internal class LinkedList
+    {
+        public Node head;
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public void DisplayBeforRotated()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+
+        public void RotateByK(int k)
+        {
+            if (head == null || k == 0)
+                return;
+
+            Node current = head;
+            int length = 1;
+
+            while (current.next != null)
+            {
+                current = current.next;
+                length++;
+            }
+
+            // make circular
+            current.next = head;
+
+            k = k % length;
+
+            if (k == 0)
+            {
+                current.next = null;
+                return;
+            }
+
+            Node newTail = head;
+
+            for (int i = 1; i < k; i++)
+            {
+                newTail = newTail.next;
+            }
+
+            head = newTail.next;
+            newTail.next = null;
+        }
+
+        public void DisplayAfterRotation()
+        {
+            DisplayBeforRotated();
+        }
+    }
+}
